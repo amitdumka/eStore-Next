b34af400d2921317211c847a5118738b02bd4c13
Amit Kumar <amitnarayansah@gmail.com>
2022-03-24 10:48:04 +0530

    commit

---
+++ aks/AKS.CharServiecs/ChatServices.cs
@@ -0,0 +1,15 @@
+﻿namespace AKS.ChatServices;
+
+/// <summary>
+/// Chat Services Setup Class
+/// </summary>
+public class ChatServices
+{
+    public bool IsEnabled { get; set; }
+    public string ServiceName { get; set; }
+    public string HostUrl { get; set; }
+    public string LicKey { get; set; }
+    public int PortNumber { get; set; }
+
+}
+

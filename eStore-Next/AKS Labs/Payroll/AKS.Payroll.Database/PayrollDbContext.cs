0681210f7a79b44ad8f8de08abe4a7b32cf3f8b6
Amit Kumar <amitnarayansah@gmail.com>
2022-03-31 10:38:35 +0530

    adding remote and local database

--- eStore-Next/AKS Labs/Payroll/AKS.Payroll.Database/PayrollDbContext.cs
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll.Database/PayrollDbContext.cs
@@ -1,33 +1,66 @@
-﻿using AKS.Shared.Payroll.Models;
+﻿using AKS.Shared.Commons.Models;
+using AKS.Shared.Payroll.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Collections;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Collections;
-using System.Configuration;
+
 //https://docs.microsoft.com/en-gb/ef/ef6/fundamentals/databinding/winforms
 namespace AKS.Payroll.Database
 {
-    public class PayrollDbContext:DbContext
+    public class LocalPayrollDbContext : PayrollDbContext
     {
-        public PayrollDbContext() { ApplyMigrations(this); }
-        public PayrollDbContext(DbContextOptions<PayrollDbContext> options) : base(options)
+        public LocalPayrollDbContext()
+        { ApplyMigrations(this); }
+
+        public LocalPayrollDbContext(DbContextOptions<PayrollDbContext> options) : base(options)
         {
             ApplyMigrations(this);
         }
-        public void ApplyMigrations(PayrollDbContext context)
+
+        public void ApplyMigrations(LocalPayrollDbContext context)
         {
             if (context.Database.GetPendingMigrations().Any())
             {
                 context.Database.Migrate();
             }
         }
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<Attendance> Attendances { get; set; }
-        public DbSet<MonthlyAttendance> MonthlyAttendances { get; set; }
-        public DbSet<Salary> Salarys { get; set; }
-        public DbSet<SalaryPayment> SalaryPayment { get; set; } 
-        public DbSet<EmployeeDetails> EmployeeDetails { get; set; }
-        public DbSet<StaffAdvanceReceipt> StaffAdvanceReceipt { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
+                //string connectionString = String.IsNullOrEmpty( ConfigurationManager.ConnectionStrings["AzureDb"].ConnectionString)? "Data Source=tcp:aprajitaretails.database.windows.net,1433;Initial Catalog=AprajitaRetails_db;User Id=AmitKumar@aprajitaretails;Password=Dumka@@2654": ConfigurationManager.ConnectionStrings["AzureDb"].ConnectionString;
+                string connectionString = "Data Source=tcp:aprajitaretails.database.windows.net,1433;Initial Catalog=AprajitaRetails_db;User Id=AmitKumar@aprajitaretails;Password=Dumka@@2654";
+
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+
+    public class AzurePayrollDbContext : PayrollDbContext
+    {
+        public AzurePayrollDbContext()
+        { ApplyMigrations(this); }
+
+        public AzurePayrollDbContext(DbContextOptions<PayrollDbContext> options) : base(options)
+        {
+            ApplyMigrations(this);
+        }
+
+        public void ApplyMigrations(AzurePayrollDbContext context)
+        {
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -37,17 +70,40 @@ namespace AKS.Payroll.Database
         {
             if (!optionsBuilder.IsConfigured)
             {
-
                 //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
                 //string connectionString = String.IsNullOrEmpty( ConfigurationManager.ConnectionStrings["AzureDb"].ConnectionString)? "Data Source=tcp:aprajitaretails.database.windows.net,1433;Initial Catalog=AprajitaRetails_db;User Id=AmitKumar@aprajitaretails;Password=Dumka@@2654": ConfigurationManager.ConnectionStrings["AzureDb"].ConnectionString;
-                string connectionString =   "Data Source=tcp:aprajitaretails.database.windows.net,1433;Initial Catalog=AprajitaRetails_db;User Id=AmitKumar@aprajitaretails;Password=Dumka@@2654" ;
+                string connectionString = "Data Source=tcp:aprajitaretails.database.windows.net,1433;Initial Catalog=AprajitaRetails_db;User Id=AmitKumar@aprajitaretails;Password=Dumka@@2654";
 
                 optionsBuilder.UseSqlServer(connectionString);
-
             }
+        }
+    }
+
+    public class PayrollDbContext : DbContext
+    {
+        public PayrollDbContext()
+        {
 
         }
 
+        public PayrollDbContext(DbContextOptions<PayrollDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<MonthlyAttendance> MonthlyAttendances { get; set; }
+        public DbSet<Salary> Salarys { get; set; }
+        public DbSet<SalaryPayment> SalaryPayment { get; set; }
+        public DbSet<EmployeeDetails> EmployeeDetails { get; set; }
+        public DbSet<StaffAdvanceReceipt> StaffAdvanceReceipt { get; set; }
+
+        //Common Table which will be shared accross Databases.
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Salesman> Salesmen { get; set; }
+
+        
     }
 
     public class ObservableListSource<T> : ObservableCollection<T>, IListSource
---
+++ eStore-Next/AKS Labs/Shared/AKS.Shared.Commons/Models/Store.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AKS.Shared.Commons.Models
+{
+    [Table("V1_Stores")]
+    public class Store
+    {
+        public int Id { get; set; }
+        [Key]
+        public string StoreId { get; set; }
+        public string StoreCode { get; set; }
+        public string StoreName { get; set; }
+        public string CompanyCode { get; set; }
+
+        public DateTime BeginDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public string StoreManager { get; set; }
+        public string StoreManagerContactNo { get; set; }
+
+        public string StorePhoneNumber { get; set; }
+        public string StoreEmailId { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+
+        public string PanNo { get; set; }
+        public string GSTIN { get; set; }
+        public string VatNo { get; set; }
+    }
+
+    [Table("V1_Salesmen")]
+    public class Salesman
+    {
+        public int Id { get; set; }
+        [Key]
+        public string SalesmanId { get; set; }
+        public string Name { get; set; }
+        public string StoreId { get; set; }
+        public string EmployeeId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}

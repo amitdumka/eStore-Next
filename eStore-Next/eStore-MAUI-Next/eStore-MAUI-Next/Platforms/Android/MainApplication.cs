53495cb6383e901b8bd9730eb71fe3028a695e92
Amit Kumar <amitnarayansah@gmail.com>
2022-04-29 02:28:21 +0530

    MAUI-Next Added

---
+++ eStore-Next/eStore-MAUI-Next/eStore-MAUI-Next/Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace eStore_MAUI_Next;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}

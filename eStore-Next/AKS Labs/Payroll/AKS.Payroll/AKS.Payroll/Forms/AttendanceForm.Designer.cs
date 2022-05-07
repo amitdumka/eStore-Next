a7401840f7c18046274e4f3c11c1fe861f523094
Amit Kumar <amitnarayansah@gmail.com>
2022-05-07 10:44:12 +0530

    bug fixed in attendance

--- eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Forms/AttendanceForm.Designer.cs
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Forms/AttendanceForm.Designer.cs
@@ -38,12 +38,13 @@
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.dgvAttendances = new System.Windows.Forms.DataGridView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.lbEmployees = new System.Windows.Forms.ListBox();
+            this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.cbAllEmployee = new System.Windows.Forms.CheckBox();
             this.btnAddAttendance = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.cbLastMonth = new System.Windows.Forms.CheckBox();
             this.panel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -60,8 +61,9 @@
             this.panel1.Controls.Add(this.tabControl1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1140, 573);
+            this.panel1.Size = new System.Drawing.Size(998, 430);
             this.panel1.TabIndex = 0;
             // 
             // tabControl1
@@ -70,9 +72,10 @@
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1140, 573);
+            this.tabControl1.Size = new System.Drawing.Size(998, 430);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -81,10 +84,11 @@
             this.tabPage1.Controls.Add(this.groupBox3);
             this.tabPage1.Controls.Add(this.groupBox2);
             this.tabPage1.Controls.Add(this.groupBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1132, 540);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tabPage1.Size = new System.Drawing.Size(990, 402);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Attendances";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -95,10 +99,11 @@
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tSSLCount,
             this.tSSLCountValue});
-            this.statusStrip1.Location = new System.Drawing.Point(137, 511);
+            this.statusStrip1.Location = new System.Drawing.Point(120, 378);
             this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 12, 0);
             this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.statusStrip1.Size = new System.Drawing.Size(992, 26);
+            this.statusStrip1.Size = new System.Drawing.Size(867, 22);
             this.statusStrip1.TabIndex = 3;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -106,23 +111,25 @@
             // 
             this.tSSLCount.Name = "tSSLCount";
             this.tSSLCount.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.tSSLCount.Size = new System.Drawing.Size(48, 20);
+            this.tSSLCount.Size = new System.Drawing.Size(40, 17);
             this.tSSLCount.Text = "Count";
             // 
             // tSSLCountValue
             // 
             this.tSSLCountValue.Name = "tSSLCountValue";
             this.tSSLCountValue.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.tSSLCountValue.Size = new System.Drawing.Size(17, 20);
+            this.tSSLCountValue.Size = new System.Drawing.Size(13, 17);
             this.tSSLCountValue.Text = "0";
             // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.dgvAttendances);
             this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox3.Location = new System.Drawing.Point(137, 77);
+            this.groupBox3.Location = new System.Drawing.Point(120, 58);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(992, 460);
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox3.Size = new System.Drawing.Size(867, 342);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Attendances";
@@ -131,11 +138,12 @@
             // 
             this.dgvAttendances.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvAttendances.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvAttendances.Location = new System.Drawing.Point(3, 23);
+            this.dgvAttendances.Location = new System.Drawing.Point(3, 18);
+            this.dgvAttendances.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvAttendances.Name = "dgvAttendances";
             this.dgvAttendances.RowHeadersWidth = 51;
             this.dgvAttendances.RowTemplate.Height = 29;
-            this.dgvAttendances.Size = new System.Drawing.Size(986, 434);
+            this.dgvAttendances.Size = new System.Drawing.Size(861, 322);
             this.dgvAttendances.TabIndex = 0;
             this.dgvAttendances.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAttendances_CellContentClick);
             // 
@@ -143,16 +151,14 @@
             // 
             this.groupBox2.Controls.Add(this.lbEmployees);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.groupBox2.Location = new System.Drawing.Point(3, 77);
+            this.groupBox2.Location = new System.Drawing.Point(3, 58);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(134, 460);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(117, 342);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Employees";
-            // 
-            // employeeBindingSource
-            // 
-            this.employeeBindingSource.DataSource = typeof(AKS.Shared.Payroll.Models.Employee);
             // 
             // lbEmployees
             // 
@@ -161,47 +167,59 @@
             this.lbEmployees.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lbEmployees.ForeColor = System.Drawing.SystemColors.HotTrack;
             this.lbEmployees.FormattingEnabled = true;
-            this.lbEmployees.ItemHeight = 20;
-            this.lbEmployees.Location = new System.Drawing.Point(3, 23);
+            this.lbEmployees.ItemHeight = 15;
+            this.lbEmployees.Location = new System.Drawing.Point(3, 18);
+            this.lbEmployees.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lbEmployees.Name = "lbEmployees";
-            this.lbEmployees.Size = new System.Drawing.Size(128, 434);
+            this.lbEmployees.Size = new System.Drawing.Size(111, 322);
             this.lbEmployees.TabIndex = 0;
             this.lbEmployees.ValueMember = "EmployeeId";
+            this.lbEmployees.SelectedIndexChanged += new System.EventHandler(this.lbEmployees_SelectedIndexChanged);
             this.lbEmployees.DoubleClick += new System.EventHandler(this.lbEmployees_DoubleClick);
+            // 
+            // employeeBindingSource
+            // 
+            this.employeeBindingSource.DataSource = typeof(AKS.Shared.Payroll.Models.Employee);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.cbLastMonth);
             this.groupBox1.Controls.Add(this.cbAllEmployee);
             this.groupBox1.Controls.Add(this.btnAddAttendance);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Location = new System.Drawing.Point(3, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1126, 74);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(984, 56);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Controls";
             // 
             // cbAllEmployee
             // 
-            this.cbAllEmployee.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.cbAllEmployee.AutoSize = true;
             this.cbAllEmployee.BackColor = System.Drawing.Color.Aquamarine;
             this.cbAllEmployee.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cbAllEmployee.Dock = System.Windows.Forms.DockStyle.Right;
             this.cbAllEmployee.Font = new System.Drawing.Font("Segoe UI", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
             this.cbAllEmployee.ForeColor = System.Drawing.Color.Red;
-            this.cbAllEmployee.Location = new System.Drawing.Point(995, 26);
+            this.cbAllEmployee.Location = new System.Drawing.Point(883, 18);
+            this.cbAllEmployee.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbAllEmployee.Name = "cbAllEmployee";
-            this.cbAllEmployee.Size = new System.Drawing.Size(125, 24);
+            this.cbAllEmployee.Size = new System.Drawing.Size(98, 36);
             this.cbAllEmployee.TabIndex = 3;
             this.cbAllEmployee.Text = "All &Employee";
             this.cbAllEmployee.UseVisualStyleBackColor = false;
+            this.cbAllEmployee.CheckedChanged += new System.EventHandler(this.cbAllEmployee_CheckedChanged);
             this.cbAllEmployee.CheckStateChanged += new System.EventHandler(this.cbAllEmployee_CheckStateChanged);
             // 
             // btnAddAttendance
             // 
-            this.btnAddAttendance.Location = new System.Drawing.Point(72, 33);
+            this.btnAddAttendance.Location = new System.Drawing.Point(63, 25);
+            this.btnAddAttendance.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnAddAttendance.Name = "btnAddAttendance";
-            this.btnAddAttendance.Size = new System.Drawing.Size(94, 29);
+            this.btnAddAttendance.Size = new System.Drawing.Size(82, 22);
             this.btnAddAttendance.TabIndex = 0;
             this.btnAddAttendance.Text = "Add";
             this.btnAddAttendance.UseVisualStyleBackColor = true;
@@ -210,22 +228,36 @@
             // tabPage2
             // 
             this.tabPage2.BackColor = System.Drawing.Color.White;
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.tabPage2.Size = new System.Drawing.Size(1132, 540);
+            this.tabPage2.Size = new System.Drawing.Size(990, 402);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Add/Update";
+            // 
+            // cbLastMonth
+            // 
+            this.cbLastMonth.AutoSize = true;
+            this.cbLastMonth.Dock = System.Windows.Forms.DockStyle.Right;
+            this.cbLastMonth.Location = new System.Drawing.Point(797, 18);
+            this.cbLastMonth.Name = "cbLastMonth";
+            this.cbLastMonth.Size = new System.Drawing.Size(86, 36);
+            this.cbLastMonth.TabIndex = 4;
+            this.cbLastMonth.Text = "Last Month";
+            this.cbLastMonth.UseVisualStyleBackColor = true;
+            this.cbLastMonth.CheckStateChanged += new System.EventHandler(this.cbLastMonth_CheckStateChanged);
             // 
             // AttendanceForm
             // 
             this.AcceptButton = this.btnAddAttendance;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(1140, 573);
+            this.ClientSize = new System.Drawing.Size(998, 430);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AttendanceForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Attendance";
@@ -264,5 +296,6 @@
         private Button btnAddAttendance;
         private BindingSource employeeBindingSource;
         private CheckBox cbAllEmployee;
+        private CheckBox cbLastMonth;
     }
 }

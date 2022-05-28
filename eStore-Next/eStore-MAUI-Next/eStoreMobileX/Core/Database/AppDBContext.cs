86284dfd7745fa78291e8daa169f8c4a4aa10c14
Amit Kumar <amitnarayansah@gmail.com>
2022-05-28 09:54:00 +0530

    remote login working

--- eStore-Next/eStore-MAUI-Next/eStoreMobileX/Core/Database/AppDBContext.cs
+++ eStore-Next/eStore-MAUI-Next/eStoreMobileX/Core/Database/AppDBContext.cs
@@ -1,6 +1,7 @@
 ﻿using AKS.Shared.Commons.Models;
 using AKS.Shared.Commons.Models.Auth;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace eStoreMobileX.Core.Database
 {
@@ -10,14 +11,14 @@ namespace eStoreMobileX.Core.Database
     public class AppDBContext : DatabaseContext
     {
         private string DatabasePath { get; set; }
-        public AppDBContext():base()
+        public AppDBContext() : base()
         {
             DatabasePath = Constants.DatabasePath;
             SQLitePCL.Batteries_V2.Init();
             this.Database.EnsureCreated();
 
         }
-        public AppDBContext(string databasePath):base()
+        public AppDBContext(string databasePath) : base()
         {
             DatabasePath = databasePath;
             SQLitePCL.Batteries_V2.Init();
@@ -36,53 +37,43 @@ namespace eStoreMobileX.Core.Database
     {
         public AzureDBContext(DbContextOptions<AzureDBContext> options) : base(options)
         {
-           // ApplyMigrations(this);
+            // ApplyMigrations(this);
         }
 
-        public AzureDBContext(string con): base( )
+        public AzureDBContext(string con) : base()
         {
 
         }
-        
+
         private static readonly string _connectionString = "Data Source=tcp:aprajitaretails.database.windows.net,1433;Initial Catalog=AprajitaRetails_db;User Id=AmitKumar@aprajitaretails;Password=Dumka@@2654";
 
-        public AzureDBContext() 
+        public AzureDBContext()
         {
         }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-        //    if (azure)
-        //        modelBuilder.HasDatabaseMaxSize("2 GB");
-        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDatabaseMaxSize("2 GB");
+        }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    if (!optionsBuilder.IsConfigured)
-        //    {
-        //        //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
-        //        //string connectionString = String.IsNullOrEmpty( ConfigurationManager.ConnectionStrings["AzureDb"].ConnectionString)? "Data Source=tcp:aprajitaretails.database.windows.net,1433;Initial Catalog=AprajitaRetails_db;User Id=AmitKumar@aprajitaretails;Password=Dumka@@2654": ConfigurationManager.ConnectionStrings["AzureDb"].ConnectionString;
-        //        optionsBuilder.UseSqlServer(ConnectionString);
-        //    }
-        //}
-
-        //public void ApplyMigrations(PayrollDbContext context)
-        //{
-        //    if (context.Database.GetPendingMigrations().Any())
-        //    {
-        //        context.Database.Migrate();
-        //    }
-        //}
+        public void ApplyMigrations(AzureDBContext context)
+        {
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+        }
 
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    if (!optionsBuilder.IsConfigured)
-        //    {
-        //        object value = optionsBuilder.UseSqlServer(_connectionString);
-        //    }
-        //}
+
     }
 
     public abstract class DatabaseContext : DbContext
@@ -95,7 +86,7 @@ namespace eStoreMobileX.Core.Database
         }
         public DatabaseContext(DbContextOptions<AzureDBContext> options) : base(options)
         {
-             
+
         }
 
     }

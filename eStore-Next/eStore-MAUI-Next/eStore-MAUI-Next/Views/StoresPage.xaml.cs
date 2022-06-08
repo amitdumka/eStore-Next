8fbc39abe95a9615b6e806bc859d1dfbad90fe62
Amit Kumar <amitnarayansah@gmail.com>
2022-06-08 09:15:32 +0530

    Store DataModel

---
+++ eStore-Next/eStore-MAUI-Next/eStore-MAUI-Next/Views/StoresPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace eStore_MAUI_Next.Views;
+
+public partial class StoresPage : ContentPage
+{
+	public StoresPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ eStore-Next/eStore-MAUI-Next/eStoreMobileX/Data/DataModels/Stores/StoreDataModel.cs
@@ -0,0 +1,37 @@
+﻿using AKS.Shared.Commons.Models;
+using eStoreMobileX.Data.DataModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStoreMobileX.Data.DataModels.Stores
+{
+    public class StoreDataModel : HybridDataMode<Store>
+
+    {
+        public StoreDataModel(ConType conType) : base(conType)
+        {
+        }
+
+        public StoreDataModel(ConType conType, string url, string name) : base(conType, url, name)
+        {
+        }
+
+        public override Task<List<Store>> FindAsync(QueryParam query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<List<Store>> GetItems(int storeid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<List<Store>> GetItems(string storeid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

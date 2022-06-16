dafe6bccd5b7232c4f9936423c17139989a981fe
Amit Kumar <amitnarayansah@gmail.com>
2022-06-16 10:50:33 +0530

    email service

---
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Ops/Mails/AKSMail.cs
@@ -0,0 +1,38 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKS.Payroll.Ops.Mails
+{
+    public static class MailConfig{
+        public const string API = "";
+        public static string From = "thearvindstoredumka@gmail.com";
+        public static string User = "Store Manager";
+    }
+
+    public class Msg
+    {
+        public string Subject { get; set; }
+        public string ToAddresses { get; set; }
+        public string Names { get; set; }
+        public string PlainContent { get; set; }
+        public string HTMLContext { get; set; }
+    }
+    public class AKSMail
+    {
+
+        public async void SendSingle(Msg msg)
+        {
+            var sendGridClient = new SendGridClient(MailConfig.API);
+            var from = new EmailAddress(MailConfig.From, MailConfig.User);
+            var to = new EmailAddress(msg.ToAddresses, msg.Names);
+            var mailMessage = MailHelper.CreateSingleEmail(from, to, msg.Subject, msg.PlainContent, msg.HTMLContext);
+            await sendGridClient.SendEmailAsync(mailMessage);
+
+        }
+    }
+}

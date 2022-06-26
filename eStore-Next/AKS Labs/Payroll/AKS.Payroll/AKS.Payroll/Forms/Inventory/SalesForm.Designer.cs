4ffa5a45742d9d09466a12d80d68761e4c4faf24
Amit Kumar <amitnarayansah@gmail.com>
2022-06-26 20:58:51 +0530

    adding sale item

--- eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Forms/Inventory/SalesForm.Designer.cs
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Forms/Inventory/SalesForm.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Syncfusion.Windows.Forms.PdfViewer.MessageBoxSettings messageBoxSettings1 = new Syncfusion.Windows.Forms.PdfViewer.MessageBoxSettings();
-            Syncfusion.Windows.PdfViewer.PdfViewerPrinterSettings pdfViewerPrinterSettings1 = new Syncfusion.Windows.PdfViewer.PdfViewerPrinterSettings();
+            Syncfusion.Windows.Forms.PdfViewer.MessageBoxSettings messageBoxSettings2 = new Syncfusion.Windows.Forms.PdfViewer.MessageBoxSettings();
+            Syncfusion.Windows.PdfViewer.PdfViewerPrinterSettings pdfViewerPrinterSettings2 = new Syncfusion.Windows.PdfViewer.PdfViewerPrinterSettings();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SalesForm));
-            Syncfusion.Windows.Forms.PdfViewer.TextSearchSettings textSearchSettings1 = new Syncfusion.Windows.Forms.PdfViewer.TextSearchSettings();
+            Syncfusion.Windows.Forms.PdfViewer.TextSearchSettings textSearchSettings2 = new Syncfusion.Windows.Forms.PdfViewer.TextSearchSettings();
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
@@ -340,6 +340,7 @@
             this.txtQty.Name = "txtQty";
             this.txtQty.Size = new System.Drawing.Size(100, 23);
             this.txtQty.TabIndex = 5;
+            this.txtQty.TextChanged += new System.EventHandler(this.txtQty_TextChanged);
             // 
             // txtProductItem
             // 
@@ -361,6 +362,7 @@
             this.txtDiscount.Name = "txtDiscount";
             this.txtDiscount.Size = new System.Drawing.Size(100, 23);
             this.txtDiscount.TabIndex = 8;
+            this.txtDiscount.TextChanged += new System.EventHandler(this.txtDiscount_TextChanged);
             // 
             // txtValue
             // 
@@ -485,16 +487,16 @@
             this.pdfViewer.IsTextSearchEnabled = true;
             this.pdfViewer.IsTextSelectionEnabled = true;
             this.pdfViewer.Location = new System.Drawing.Point(3, 3);
-            messageBoxSettings1.EnableNotification = true;
-            this.pdfViewer.MessageBoxSettings = messageBoxSettings1;
+            messageBoxSettings2.EnableNotification = true;
+            this.pdfViewer.MessageBoxSettings = messageBoxSettings2;
             this.pdfViewer.MinimumZoomPercentage = 50;
             this.pdfViewer.Name = "pdfViewer";
             this.pdfViewer.PageBorderThickness = 1;
-            pdfViewerPrinterSettings1.PageOrientation = Syncfusion.Windows.PdfViewer.PdfViewerPrintOrientation.Auto;
-            pdfViewerPrinterSettings1.PageSize = Syncfusion.Windows.PdfViewer.PdfViewerPrintSize.ActualSize;
-            pdfViewerPrinterSettings1.PrintLocation = ((System.Drawing.PointF)(resources.GetObject("pdfViewerPrinterSettings1.PrintLocation")));
-            pdfViewerPrinterSettings1.ShowPrintStatusDialog = true;
-            this.pdfViewer.PrinterSettings = pdfViewerPrinterSettings1;
+            pdfViewerPrinterSettings2.PageOrientation = Syncfusion.Windows.PdfViewer.PdfViewerPrintOrientation.Auto;
+            pdfViewerPrinterSettings2.PageSize = Syncfusion.Windows.PdfViewer.PdfViewerPrintSize.ActualSize;
+            pdfViewerPrinterSettings2.PrintLocation = ((System.Drawing.PointF)(resources.GetObject("pdfViewerPrinterSettings2.PrintLocation")));
+            pdfViewerPrinterSettings2.ShowPrintStatusDialog = true;
+            this.pdfViewer.PrinterSettings = pdfViewerPrinterSettings2;
             this.pdfViewer.ReferencePath = null;
             this.pdfViewer.ScrollDisplacementValue = 0;
             this.pdfViewer.ShowHorizontalScrollBar = true;
@@ -502,10 +504,10 @@
             this.pdfViewer.Size = new System.Drawing.Size(875, 314);
             this.pdfViewer.SpaceBetweenPages = 8;
             this.pdfViewer.TabIndex = 0;
-            textSearchSettings1.CurrentInstanceColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(255)))), ((int)(((byte)(171)))), ((int)(((byte)(64)))));
-            textSearchSettings1.HighlightAllInstance = true;
-            textSearchSettings1.OtherInstanceColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(254)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
-            this.pdfViewer.TextSearchSettings = textSearchSettings1;
+            textSearchSettings2.CurrentInstanceColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(255)))), ((int)(((byte)(171)))), ((int)(((byte)(64)))));
+            textSearchSettings2.HighlightAllInstance = true;
+            textSearchSettings2.OtherInstanceColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(254)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.pdfViewer.TextSearchSettings = textSearchSettings2;
             this.pdfViewer.ThemeName = "Default";
             this.pdfViewer.VerticalScrollOffset = 0;
             this.pdfViewer.VisualStyle = Syncfusion.Windows.Forms.PdfViewer.VisualStyle.Default;
--- eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Forms/Inventory/SalesForm.cs
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Forms/Inventory/SalesForm.cs
@@ -1,15 +1,7 @@
 ﻿using AKS.Payroll.Database;
 using AKS.Shared.Commons.Models.Inventory;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace AKS.Payroll.Forms.Inventory
 {
@@ -25,9 +17,11 @@ namespace AKS.Payroll.Forms.Inventory
         private int SeletedYear;
         private List<int> YearList;
         private bool IsNew;
+        private bool ReturnKey = false;
 
-        // Cart Information 
+        // Cart Information
         private decimal TotalQty, TotalFreeQty, TotalTax, TotalDiscount, TotalAmount;
+
         private int TotalCount;
 
         public SalesForm()
@@ -35,6 +29,7 @@ namespace AKS.Payroll.Forms.Inventory
             InitializeComponent();
             InvoiceType = InvoiceType.ManualSale;
         }
+
         public SalesForm(InvoiceType type)
         {
             InitializeComponent();
@@ -49,27 +44,30 @@ namespace AKS.Payroll.Forms.Inventory
                     rbRegular.Checked = true;
                     this.Text = "Regular Invoice";
                     break;
+
                 case InvoiceType.SalesReturn:
                     rbRegular.Checked = true;
                     cbSalesReturn.Checked = true;
                     this.Text = "Regular Sale's Invoice";
                     break;
+
                 case InvoiceType.ManualSale:
                     rbManual.Checked = true;
                     this.Text = "Manual Invoice";
                     break;
+
                 case InvoiceType.ManualSaleReturn:
                     rbManual.Checked = true;
                     cbSalesReturn.Checked = true;
                     this.Text = "Manual Sale's Return Invoice";
                     break;
+
                 default:
                     rbManual.Checked = true;
                     this.Text = "Manual Invoice";
                     break;
             }
         }
-
 
         private void rbRegular_CheckedChanged(object sender, EventArgs e)
         {
@@ -80,7 +78,6 @@ namespace AKS.Payroll.Forms.Inventory
                 else
                     InvoiceType = InvoiceType.Sales;
             }
-
         }
 
         private void rbManual_CheckedChanged(object sender, EventArgs e)
@@ -136,12 +133,13 @@ namespace AKS.Payroll.Forms.Inventory
                 .ToList());
 
             dataGridView1.DataSource = Items.Where(c => c.InvoiceType == InvoiceType).ToList();
-
         }
+
         private void SetGridView()
         {
             dataGridView1.DataSource = Items.Where(c => c.InvoiceType == InvoiceType).ToList();
         }
+
         private void SalesForm_Load(object sender, EventArgs e)
         {
             LoadData();
@@ -149,12 +147,14 @@ namespace AKS.Payroll.Forms.Inventory
 
         private void LoadFormData()
         {
-            cbxMmobile.DataSource = azureDb.Customers.Select(c => new { c.MobileNo, c.CustomerName, c.FirstName }).OrderBy(c=> c.FirstName).ToList();
+            cbxMmobile.DataSource = azureDb.Customers.Select(c => new { c.MobileNo, c.CustomerName, c.FirstName }).OrderBy(c => c.FirstName).ToList();
             cbxMmobile.DisplayMember = "MobileNo";
             cbxMmobile.ValueMember = "CustomerName";
 
             cbxInvType.Items.AddRange(Enum.GetNames(typeof(InvoiceType)));
-
+            
+            SaleItem = new ObservableListSource<SaleItemVM>();
+            dgvSaleItems.DataSource = SaleItem;
         }
 
         private void cbxMmobile_SelectedIndexChanged(object sender, EventArgs e)
@@ -169,47 +169,47 @@ namespace AKS.Payroll.Forms.Inventory
 
         private void txtBarcode_TextChanged(object sender, EventArgs e)
         {
-            if (ReturnKey)
-            {
-                MessageBox.Show(txtBarcode.Text);
-                ReturnKey = false;
-            }
+            //if (ReturnKey)
+            //{
+            //    MessageBox.Show(txtBarcode.Text);
+            //    ReturnKey = false;
+            //}
         }
-        private bool ReturnKey = false;
+
         private void txtBarcode_KeyPress(object sender, KeyPressEventArgs e)
         {
             // if(e.KeyChar==Keys.Enter)
-            MessageBox.Show(e.KeyChar + "");
+            // MessageBox.Show(e.KeyChar + "");
         }
 
         private void txtBarcode_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Enter)
-            {
-                MessageBox.Show("Enter Is Pressed!");
-                ReturnKey = true;
-            }
-
+            //if (e.KeyCode == Keys.Enter)
+            //{
+            //    MessageBox.Show("Enter Is Pressed!");
+            //    ReturnKey = true;
+            //}
         }
 
         private void txtBarcode_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
         {
             if (e.KeyCode == Keys.Tab)
             {
-                MessageBox.Show("Tab is pressed");
+                MessageBox.Show("Tab is pressed\n" + txtBarcode.Text.Trim());
                 ReturnKey = true;
+                HandleBarcodeEntry();
             }
             else if (e.KeyCode == Keys.Enter)
             {
-                MessageBox.Show("Enter");
+                MessageBox.Show("Enter\n" + txtBarcode.Text.Trim());
                 ReturnKey = true;
+                HandleBarcodeEntry();
             }
-
         }
 
         private void btnAddCustomer_Click(object sender, EventArgs e)
         {
-            // Show Dailog to Add Customer. 
+            // Show Dailog to Add Customer.
         }
 
         private void ResetCard()
@@ -217,46 +217,130 @@ namespace AKS.Payroll.Forms.Inventory
             this.TotalAmount = this.TotalDiscount = this.TotalTax = TotalQty = TotalFreeQty = 0;
             TotalCount = 0;
             dgvSaleItems.Rows.Clear();
-
         }
+
         private void BasicRateCalucaltion(decimal mrp, decimal taxRate)
         {
-            decimal price = (100*mrp)/(100+taxRate);
+            decimal price = (100 * mrp) / (100 + taxRate);
             decimal taxAmount = mrp - price;
         }
 
         private void UpdateCart()
         {
-
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            LoadFormData();
-        }
+            try
+            {
+                LoadFormData();
+            }
+            catch (Exception ex)
+            {
 
+                MessageBox.Show(ex.Message);
+            }
+           
+
+
+        }
+        private ObservableListSource<SaleItemVM> SaleItem;
         private void DisplayStockInfo(StockInfo info)
         {
             txtQty.Text = info.Qty.ToString();
             txtRate.Text = info.Rate.ToString();
-            txtValue.Text = (info.Qty * info.Rate).ToString(); 
-            txtProductItem.Text=info.ProductItem.ToString();
+            txtValue.Text = (info.Qty * info.Rate).ToString();
+            txtProductItem.Text = info.ProductItem.ToString();
+            txtDiscount.Text = "0";
         }
+
+        private void txtQty_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtDiscount.Text.Contains('%'))
+                {
+                    var x = (decimal.Parse(txtQty.Text.Trim()) * decimal.Parse(txtRate.Text.Trim()));
+                    x -= x * decimal.Parse(txtDiscount.Text.Replace('%', ' ').Trim()) / 100;
+                    txtValue.Text = x.ToString();
+                }
+                else
+                {
+                    var x = (decimal.Parse(txtQty.Text.Trim()) * decimal.Parse(txtRate.Text.Trim()))
+                        - decimal.Parse(txtDiscount.Text.Trim());
+                    txtValue.Text = x.ToString();
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void txtDiscount_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtDiscount.Text.Contains('%'))
+                {
+                    var x = (decimal.Parse(txtQty.Text.Trim()) * decimal.Parse(txtRate.Text.Trim()));
+                    x -= x * decimal.Parse(txtDiscount.Text.Replace('%', ' ').Trim()) / 100;
+                    txtValue.Text = x.ToString();
+                }
+                else
+                {
+                    var x = (decimal.Parse(txtQty.Text.Trim()) * decimal.Parse(txtRate.Text.Trim()))
+                        - decimal.Parse(txtDiscount.Text.Trim());
+                    txtValue.Text = x.ToString();
+                }
+            }
+            catch
+            {
+            }
+        }
+
         private void AddToCart()
         {
-            var si = new SaleItemVM
+            try
             {
-                Barcode = txtBarcode.Text.Trim(),
-                Rate = decimal.Parse(txtRate.Text.Trim()),
-                Discount = decimal.Parse(txtDiscount.Text.Trim()),
-                ProductItem = txtProductItem.Text.Trim(),
-                Qty = decimal.Parse(txtQty.Text.Trim()),
-                Amount = 0,
-                Tax = 0,
-            };
-            si.Amount = (si.Rate * si.Qty) - si.Discount;
+                var si = new SaleItemVM
+                {
+                    Barcode = txtBarcode.Text.Trim(),
+                    Rate = decimal.Parse(txtRate.Text.Trim()),
+                    ProductItem = txtProductItem.Text.Trim(),
+                    Qty = decimal.Parse(txtQty.Text.Trim()),
+                    Amount = 0,
+                    Tax = 0,
+                };
 
+                if (txtDiscount.Text.Contains('%'))
+                {
+                    si.Discount = si.Qty * si.Rate * decimal.Parse(txtDiscount.Text.Replace('%', ' ').Trim()) / 100;
+                }
+                else
+                    si.Discount = decimal.Parse(txtDiscount.Text.Trim());
+
+                si.Amount = (si.Rate * si.Qty) - si.Discount;
+
+                SaleItem.Add(si);
+                txtBarcode.Text ="";
+                txtQty.Text = "0";
+                txtRate.Text = "0";
+                txtProductItem.Text = "";
+                txtDiscount.Text = "0";
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
+
+        private void HandleBarcodeEntry()
+        {
+            if (ReturnKey)
+                DisplayStockInfo(GetItemDetail(txtBarcode.Text.Trim()));
+        }
+
         /// <summary>
         /// return stock info. Add to API/DataModel
         /// </summary>
@@ -280,17 +364,19 @@ namespace AKS.Payroll.Forms.Inventory
                TaxType = item.Product.TaxType,
                Unit = item.Product.Unit,
                Category = item.Product.ProductCategory,
-               TaxRate = SetTaxRate(item.Product.ProductCategory,item.Product.MRP)
+               TaxRate = SetTaxRate(item.Product.ProductCategory, item.Product.MRP)
            }).FirstOrDefault();
             return item;
         }
+
         /// <summary>
         /// make static and common functions
         /// </summary>
         /// <param name="category"></param>
         /// <param name="Price"></param>
         /// <returns></returns>
-        private int SetTaxRate(ProductCategory category, decimal Price)
+
+        private static int SetTaxRate(ProductCategory category, decimal Price)
         {
             int rate = 0;
             switch (category)
@@ -298,37 +384,45 @@ namespace AKS.Payroll.Forms.Inventory
                 case ProductCategory.Fabric:
                     rate = 5;
                     break;
+
                 case ProductCategory.ReadyMade:
                     rate = Price > 999 ? 12 : 5;
                     break;
+
                 case ProductCategory.Accessiories:
                     rate = 12;
                     break;
+
                 case ProductCategory.Tailoring:
                     rate = 5;
                     break;
+
                 case ProductCategory.Trims:
                     rate = 5;
                     break;
+
                 case ProductCategory.PromoItems:
                     rate = 0;
                     break;
+
                 case ProductCategory.Coupons:
                     rate = 0;
                     break;
+
                 case ProductCategory.GiftVouchers:
                     rate = 0;
                     break;
+
                 case ProductCategory.Others:
                     rate = 18;
                     break;
+
                 default:
                     rate = 5;
                     break;
             }
             return rate;
         }
-
     }
 
     public class StockInfo

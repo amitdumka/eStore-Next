12802a016b7c4022fe9f25ae364c8227e39e9f8f
Amit Kumar <amitnarayansah@gmail.com>
2022-06-29 10:37:42 +0530

    sql querries

---
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/SQLQuery1213.sql
@@ -0,0 +1,9 @@
+﻿--select A.VendorId , VendorName from  V1_PurchaseProducts as A Inner Join V1_Vendors  AS B On
+--B.VendorId=A.VendorId
+--Group By A.VendorId , VendorName 
+--select * from V1_Vendors
+
+select *,B.VendorId from  V1_PurchaseItems As A Inner Join 
+V1_PurchaseProducts As B On B.InwardNumber=A.InwardNumber
+where B.VendorId='1'
+select * from V1_Vendors
---
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/TableNameQuerry.sql
@@ -0,0 +1,22 @@
+﻿--https://docs.microsoft.com/en-us/answers/questions/364258/how-should-i-get-a-list-of-table-names-and-field-n.html
+SELECT
+      s.name AS SchemaName
+     ,t.name AS TableName
+     ,c.name AS ColumnName
+ FROM sys.schemas AS s
+ JOIN sys.tables AS t ON t.schema_id = s.schema_id
+ JOIN sys.columns AS c ON c.object_id = t.object_id
+ ORDER BY
+      SchemaName
+     ,TableName
+     ,ColumnName;
+    
+ SELECT
+      TABLE_SCHEMA AS SchemaName
+     ,TABLE_NAME AS TableName
+     ,COLUMN_NAME AS ColumnName
+ FROM INFORMATION_SCHEMA.COLUMNS
+ ORDER BY
+      SchemaName
+     ,TableName
+     ,ColumnName;

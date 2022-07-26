6a9509c08448904a3c27417b20585671bb672d40
Amit Kumar <amitnarayansah@gmail.com>
2022-07-26 11:13:02 +0530

    inventory

---
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Forms/Inventory/InventoryManager.cs
@@ -0,0 +1,161 @@
+namespace AKS.Payroll.Forms.Inventory
+{
+    public class InventoryManager
+    {
+        public AzurePayrollDbContext azureDb;
+        public LocalPayrollDbContext localDb;
+        public string StoreCode;
+        public InventoryManager(AzurePayrollDbContext db, LocalPayrollDbContext ldb, string sc)
+        {
+            azureDb = db; localDb = ldb; StoreCode = sc;
+        }
+        public void CreateCategoryList(DataTable dataTable)
+        {
+            var data = dataTable.AsEnumerable().GroupBy(c => c["Product Name"]).Select(c => c.Key.ToString()).ToList();
+            List<string> a = new List<string>();
+            List<string> b = new List<string>();
+            List<string> c = new List<string>();
+
+            foreach (var item in data)
+            {
+                var x = item.Split("/");
+                a.Add(x[0]);
+                b.Add(x[1]);
+                c.Add(x[2]);
+            }
+
+            a = a.Distinct().ToList();
+            b = b.Distinct().ToList();
+            c = c.Distinct().ToList();
+            c.Sort();
+
+            // Apprel, shirting, suiting
+            listBox1.DataSource = a;
+            // Color or stle name
+            listBox2.DataSource = b;
+            // Type of Product. 
+            listBox3.DataSource = c;
+            //Store In table.
+        }
+        //Set Unit size based on category
+        public static Unit SetUnit(string name)
+        {
+            if (name.StartsWith("Apparel")) { return Unit.Pcs; }
+            else if (name.StartsWith("Promo") || name.StartsWith("Suit Cover")) { return Unit.Nos; }
+            else if (name.StartsWith("Shirting") || name.StartsWith("Suiting")) { return Unit.Meters; }
+            return Unit.NoUnit;
+        }
+        public static global::Size SetSize(string style, string category)
+        {
+            global::Size size;
+            var name = style.Substring(style.Length - 4, 4);
+            var enumList = Enum.GetNames(typeof(global::Size));
+            // Jeans and Trousers
+            if (category.Contains("Jeans") || category.Contains("Trousers"))
+            {
+                size = (global::Size)enumList.IndexOf($"T{name[2]}{name[3]}");
+            }
+            else if (category.Contains("Shirt") || category.Contains("TShirt"))
+            {
+                if (name.EndsWith(global::Size.XXXL.ToString()))
+                    size = global::Size.XXXL;
+                else if (name.EndsWith(global::Size.XXL.ToString())) { size = global::Size.XXL; }
+                else if (name.EndsWith(global::Size.XL.ToString())) { size = global::Size.XL; }
+                else if (name.EndsWith(global::Size.L.ToString())) { size = global::Size.L; }
+                else if (name.EndsWith(global::Size.M.ToString())) { size = global::Size.M; }
+                else if (name.EndsWith(global::Size.S.ToString())) { size = global::Size.S; }
+
+            }
+            return size;
+        }
+        private void ProcessProductItem()
+        {
+            for (int i = 0; i < DataTable.Rows.Count; i++)
+
+            {
+                if (!ProuctItemExist(DataTable.Rows[i]["Barcode"].ToString()))
+                {
+                    ProductItem item = new ProductItem
+                    {
+                        Barcode = DataTable.Rows[i]["Barcode"].ToString(),
+                        StyleCode = DataTable.Rows[i]["StyleCode"].ToString(),
+                        Description = DataTable.Rows[i]["Item Desc"].ToString(),
+                        MRP = decimal.Parse(DataTable.Rows[i]["Item Desc"].ToString().Trim()),
+                        HSNCode = "",
+                        Name = DataTable.Rows[i]["Product Name"].ToString(),
+                        TaxType = TaxType.GST,
+                        Unit = SetUnit(DataTable.Rows[i]["Product Name"].ToString()),
+                    };
+                    var names = DataTable.Rows[i]["Product Name"].ToString().Split("/");
+                    if (item.Unit == Unit.Meters || item.Unit == Unit.Nos || item.Unit == Unit.NoUnit) item.Size = global::Size.NS;
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+                    DataTable.Rows[i]["ExmilCost"] = "Exist";
+                }
+            }
+        }
+        private void ProcessStockUpdate()
+        {
+            var stock = azureDb.PurchaseItems
+                .Select(c => new Stock
+                {
+                    Barcode = c.Barcode,
+                    CostPrice = c.CostPrice,
+                    EntryStatus = EntryStatus.Added,
+                    IsReadOnly = true,
+                    MarkedDeleted = false,
+                    StoreId = "ARD",
+                    HoldQty = 0,
+                    SoldQty = 0,
+                    Unit = c.Unit,
+                    UserId = "AUTO",
+                    PurhcaseQty = c.Qty
+                }).ToList();
+
+            List<Stock> Stocks = new List<Stock>();
+            //search for dplicate barcode
+
+            //var dup = stock.GroupBy(c => c.Barcode).Select(c=> new {c.Key , ctr=c.Key.Count()}).Where(c => c.ctr > 1).ToList(); 
+            foreach (var item in stock)
+            {
+                if (Stocks.Any(c => c.Barcode == item.Barcode))
+                {
+                    var s = Stocks.Find(c => c.Barcode == item.Barcode);
+                    Stocks.Remove(s);
+                    s.PurhcaseQty += item.PurhcaseQty;
+                    Stocks.Add(s);
+                }
+                else
+                {
+                    Stocks.Add(item);
+
+                }
+
+            }
+            azureDb.Stocks.AddRange(Stocks);
+            var c = azureDb.SaveChanges();
+            if (c > 0)
+            {
+                Console.WriteLine(c);
+            }
+        }
+        public bool ProuctItemExist(string barcode)
+        {
+            return azureDb.ProductItems.Any(c => c.Barcode == barcode);
+        }
+
+    }
+
+    public class Utils
+    {
+        public static int ReadInt(TextBox t)
+        {
+            return Int32.Parse(t.Text.Trim());
+        }
+    }
+}
--- eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Forms/Inventory/PurchaseForm.cs
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Forms/Inventory/PurchaseForm.cs
@@ -19,6 +19,7 @@ namespace AKS.Payroll.Forms.Inventory
         private AzurePayrollDbContext azureDb;
         private LocalPayrollDbContext localDb;
         DataTable DataTable;
+        InventoryManager _im;
         public PurchaseForm()
         {
             InitializeComponent();
@@ -28,24 +29,24 @@ namespace AKS.Payroll.Forms.Inventory
         {
 
         }
-
         private void PurchaseForm_Load(object sender, EventArgs e)
         {
-            azureDb= new AzurePayrollDbContext();
+            azureDb = new AzurePayrollDbContext();
+            _im = new InventoryManager(azureDb, localDb, "ARD");
         }
         public void LoadStock()
         {
-            if (azureDb.Stocks.Local == null && azureDb.Stocks.Local.Count<=0)
+            if (azureDb.Stocks.Local == null && azureDb.Stocks.Local.Count <= 0)
                 azureDb.Stocks.Load();
 
             dgvPurchase.DataSource = azureDb.Stocks.Local.ToBindingList();
         }
         public void LoadInvoice()
         {
-            if(  azureDb.PurchaseItems.Local.Count<=0)
-            azureDb.PurchaseItems.Load();
+            if (azureDb.PurchaseItems.Local.Count <= 0)
+                azureDb.PurchaseItems.Load();
 
-            dgvPurchase.DataSource = azureDb.PurchaseItems.Local.OrderBy(c=>c.Barcode).ToList(); 
+            dgvPurchase.DataSource = azureDb.PurchaseItems.Local.OrderBy(c => c.Barcode).ToList();
         }
 
         private void rbInvoices_CheckedChanged(object sender, EventArgs e)
@@ -56,150 +57,26 @@ namespace AKS.Payroll.Forms.Inventory
 
         private void rbStocks_CheckedChanged(object sender, EventArgs e)
         {
-            if(rbStocks.Checked) LoadStock();
+            if (rbStocks.Checked) LoadStock();
         }
 
-        private void ProcessStockUpdate()
-        {
-            var stock = azureDb.PurchaseItems                
-                .Select(c => new Stock { Barcode=c.Barcode, CostPrice=c.CostPrice, EntryStatus=EntryStatus.Added,
-                 IsReadOnly=true, MarkedDeleted=false, StoreId="ARD", HoldQty=0, SoldQty=0, Unit=c.Unit, UserId="AUTO", PurhcaseQty=c.Qty
-            }).ToList();
-
-            List<Stock> Stocks = new List<Stock>(); 
-            //search for dplicate barcode
-
-            //var dup = stock.GroupBy(c => c.Barcode).Select(c=> new {c.Key , ctr=c.Key.Count()}).Where(c => c.ctr > 1).ToList(); 
-            foreach (var item in stock)
-            {
-                if (Stocks.Any(c => c.Barcode == item.Barcode))
-                {
-                    var s = Stocks.Find(c => c.Barcode == item.Barcode);
-                    Stocks.Remove(s);
-                    s.PurhcaseQty += item.PurhcaseQty;
-                    Stocks.Add(s); 
-                }
-                else
-                {
-                    Stocks.Add(item); 
-
-                }
-
-            }
-            azureDb.Stocks.AddRange(Stocks);
-            var c = azureDb.SaveChanges();
-            if (c > 0)
-            {
-                Console.WriteLine(c);
-            }
-            
-        }
 
         private void btnRefresh_Click(object sender, EventArgs e)
         {
-            ProcessStockUpdate(); 
+
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-           DataTable = ImportData.ReadExcelToDatatable(txtBarcode.Text.Trim(), ReadInt(txtQty ), ReadInt(txtProductItem), ReadInt(txtRate), ReadInt(txtDiscount));
+            DataTable = ImportData.ReadExcelToDatatable(txtBarcode.Text.Trim(), ReadInt(txtQty), ReadInt(txtProductItem), ReadInt(txtRate), ReadInt(txtDiscount));
 
-            dgvPurchase.DataSource = DataTable; 
+            dgvPurchase.DataSource = DataTable;
 
-        }
-        private void CreateCategoryList()
-        {
-            var data = DataTable.AsEnumerable().GroupBy(c => c["Product Name"] ).Select(c=>c.Key.ToString()).ToList();
-            List<string> a = new List<string>();
-            List<string> b = new List<string>();
-            List<string> c = new List<string>();
-            foreach (var item in data)
-            {
-                var x = item.Split("/");
-                a.Add(x[0]); 
-                b.Add(x[1]);
-                c.Add(x[2]);
-            }
-            a=a.Distinct().ToList();
-            b=b.Distinct().ToList();
-            c = c.Distinct().ToList();
-            c.Sort();
-            listBox1.DataSource = a;
-            listBox2.DataSource = b; 
-            listBox3.DataSource = c;
-        }
-
-        private int ReadInt(TextBox t)
-        {
-            return Int32.Parse(t.Text.Trim());
-        }
-        private Unit SetUnit(string name)
-        {
-            if (name.StartsWith("Apparel")) { return Unit.Pcs; }
-            else if (name.StartsWith("Promo") || name.StartsWith("Suit Cover") ) { return Unit.Nos; }
-            else if (name.StartsWith("Shirting") || name.StartsWith("Suiting")) { return Unit.Meters; }
-            return Unit.NoUnit;
-        }
-        private void SetSize(string style,string category)
-        {
-            global::Size size;
-            var name = style.Substring(style.Length - 4, 4);
-            //if (name[1] == 'T')
-            //{
-            //   name= name.Substring(1);
-            //   size= (global::Size) Enum.GetNames(typeof(global::Size)).ToList().IndexOf(name);
-            //}
-            if (name.EndsWith(global::Size.XXXL.ToString()))
-            {
-
-            }
-            else if (name.EndsWith(global::Size.XXL.ToString())) { }
-            else if (name.EndsWith(global::Size.XL.ToString())) { }
-            else if (name.EndsWith(global::Size.L.ToString())) { }
-            else if (name.EndsWith(global::Size.M.ToString())) { }
-            else if (name.EndsWith(global::Size.S.ToString())) { }
-            
-            else if (name.EndsWith(global::Size.T28.ToString())) { }
-            else if (name.EndsWith(global::Size.XXXL.ToString())) { }
-            else if (name.EndsWith(global::Size.XXXL.ToString())) { }
-            else if (name.EndsWith(global::Size.XXXL.ToString())) { }
-        }
-        private void ProcessProductItem()
-        {
-            for (int i = 0; i < DataTable.Rows.Count; i++)
-             
-            {
-                if (!ProuctItemExist(DataTable.Rows[i]["Barcode"].ToString()))
-                {
-                    ProductItem item = new ProductItem {
-                        Barcode = DataTable.Rows[i]["Barcode"].ToString(),
-                        StyleCode = DataTable.Rows[i]["StyleCode"].ToString(),
-                        Description = DataTable.Rows[i]["Item Desc"].ToString(),
-                        MRP = decimal.Parse(DataTable.Rows[i]["Item Desc"].ToString().Trim()),
-                        HSNCode = "",Name= DataTable.Rows[i]["Product Name"].ToString(), TaxType=TaxType.GST, Unit=SetUnit(DataTable.Rows[i]["Product Name"].ToString()), 
-                    };
-                    var names = DataTable.Rows[i]["Product Name"].ToString().Split("/");
-                    if (item.Unit == Unit.Meters || item.Unit==Unit.Nos||item.Unit==Unit.NoUnit) item.Size = global::Size.NS;
-                    else
-                    {
-
-                    }
-                }
-                else
-                {
-                    DataTable.Rows[i]["ExmilCost"] = "Exist";
-                }
-            }
-        }
-
-        public bool ProuctItemExist(string barcode)
-        {
-            return azureDb.ProductItems.Any(c => c.Barcode == barcode);
         }
 
         private void btnCancle_Click(object sender, EventArgs e)
         {
-            CreateCategoryList(); 
+
 
         }
     }

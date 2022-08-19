38b10c7048a3f75130b69e5709ccccfd570461ce
Amit Kumar <amitnarayansah@gmail.com>
2022-08-19 10:57:18 +0530

    sale report

--- eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Ops/SaleHelper.cs
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.Payroll/Ops/SaleHelper.cs
@@ -60,15 +60,50 @@ namespace AKS.Payroll.Ops
                 dataTable.Columns.Add("Bill Qty");
                 dataTable.Columns.Add("Free Qty");
                 dataTable.Columns.Add("MRP Value");
+                dataTable.Columns.Add("Discount Value");
                 dataTable.Columns.Add("Tax Value");
                 dataTable.Columns.Add("Invoice Value");
 
                 //Do group based on year and month then make sub total . 
                 //then year wise total 
                 //then total  total
-                foreach (var item in saleReports)
+                var reps = saleReports.GroupBy(c => new { c.Year, c.Month }).ToList();
+
+                int curYear= reps[0].Key.Year;
+                foreach (var item in reps)
                 {
-                    dataTable.Rows.Add(new object[] {});
+                    if (curYear != item.Key.Year)
+                    {
+                        var yrData= saleReports.Where(c =>  c.Year == curYear).
+                          GroupBy(c=>c.Year)  
+                         .Select(c=>new {c.Key, Tax=c.Sum(x=>x.TotalTax),
+                             MRP = c.Sum(x => x.TotalMRP),
+                             Discount = c.Sum(x => x.TotalDiscount),
+                             Qty = c.Sum(x => x.BillQty),
+                             Value = c.Sum(x => x.TotalPrice),
+                             Free = c.Sum(x => x.FreeQty),
+                         })  
+                          .  FirstOrDefault();
+                        dataTable.Rows.Add(new object[] {curYear, "Yearly Total", "Sale",
+                            yrData.Qty, yrData.Free, yrData.MRP,yrData.Discount, yrData.Tax, yrData.Value    });
+                        // Add a blank if needed
+                        curYear=item.Key.Year;
+
+                    }
+                    var datas = saleReports.Where(c => c.Month == item.Key.Month && c.Year == item.Key.Year).ToList();
+
+                    foreach (var sale in datas)
+                    {
+                        dataTable.Rows.Add(new object[] {sale.Year, sale.Month, sale.InvoiceType,
+                            sale.BillQty, sale.FreeQty, sale.TotalMRP,sale.TotalDiscount, sale.TotalTax, sale.TotalPrice });
+                    }
+                    if (datas.Count() > 0)
+                    {
+                        dataTable.Rows.Add(new object[] {item.Key.Year, item.Key.Month, "Sale",
+                            datas.Sum(c=>c.BillQty),  datas.Sum(c=>c.FreeQty),datas.Sum(c=>c.TotalMRP), datas.Sum(c=>c.TotalDiscount),
+                             datas.Sum(c=>c.TotalTax),  datas.Sum(c=>c.TotalPrice) });
+                    }
+                    
                 }
 
 
@@ -80,7 +115,7 @@ namespace AKS.Payroll.Ops
                 Console.WriteLine(e.Message);
                 return dataTable;
             }
-            
+
 
         }
         public string ToPdf()

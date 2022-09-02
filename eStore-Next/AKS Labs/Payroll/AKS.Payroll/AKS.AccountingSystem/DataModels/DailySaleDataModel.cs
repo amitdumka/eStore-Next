176f60829855158f2497765aaeb00dae13d16b3b
Amit Kumar <amitnarayansah@gmail.com>
2022-09-02 11:44:22 +0530

    dailysale

---
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.AccountingSystem/DataModels/DailySaleDataModel.cs
@@ -0,0 +1,58 @@
+﻿using AKS.Shared.Commons.Models.Sales;
+using AKS.Shared.Templets.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKS.AccountingSystem.DataModels
+{
+    public class DailySaleDataModel : DataModel<DailySale, CustomerDue, DueRecovery>
+    {
+        public override DailySale Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DailySale Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<DailySale> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DueRecovery GetSeconday(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DueRecovery GetSeconday(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<DueRecovery> GetSecondayList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override CustomerDue GetY(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override CustomerDue GetY(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<CustomerDue> GetYList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/AKS.AccountingSystem/ViewModels/DailySaleViewModel.cs
@@ -0,0 +1,106 @@
+﻿using AKS.AccountingSystem.DataModels;
+using AKS.Payroll.Database;
+using AKS.Shared.Commons.Models.Sales;
+using AKS.Shared.Templets.ViewModels;
+
+namespace AKS.AccountingSystem.ViewModels
+{
+    public class DailySaleViewModel : ViewModel<DailySale, CustomerDue, DailySaleVM, DailySaleDataModel>
+    {
+        //TODO: Use this class for CustomerDue entry  and for listing and editing for dues use dueviewmodel
+        #region Declarations
+        //private AzurePayrollDbContext azureDb;
+        //private LocalPayrollDbContext localDb;
+        private ObservableListSource<DailySaleVM> dailySaleVMs;//PrimaryVM
+        private List<int> YearList;
+        private List<int> YearDataList;
+        private static string StoreCode = "ARD";
+        private List<DueRecovery> dueRecoveryList;//
+        private List<CustomerDue> customerDues;//Secondary
+        #endregion
+        #region OverrideMethods
+
+        public override bool Delete(DailySale entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Delete(CustomerDue entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRange(List<DailySale> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRange(List<CustomerDue> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool InitViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Save(DailySale entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Save(CustomerDue entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion OverrideMethods
+    }
+
+
+    public class DueViewModel : ViewModel<CustomerDue, DueRecovery, DailySaleDataModel>
+    {
+        #region Decalarations
+        //Primary Enity Due, Seconday Enity Recovery
+        #endregion
+        #region OverrideMethods
+
+
+        public override bool Delete(CustomerDue entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Delete(DueRecovery entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRange(List<CustomerDue> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRange(List<DueRecovery> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool InitViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Save(CustomerDue entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Save(DueRecovery entity)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}

227321b838f4cce1563d0390bd52023c9707ba0a
Amit Kumar <amitnarayansah@gmail.com>
2022-09-03 15:05:40 +0530

    Mobile App

--- eStore-Next/Mobiles/eStore-Maui/AppShell.xaml.cs
+++ eStore-Next/Mobiles/eStore-Maui/AppShell.xaml.cs
@@ -1,10 +1,26 @@
-﻿namespace eStore_Maui
+﻿using System.Diagnostics;
+
+namespace eStore_Maui
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            if (DeviceInfo.Idiom == DeviceIdiom.Phone)
+                AppShell.Current.CurrentItem = PhoneTabs;
+        }
+        async void TapGestureRecognizer_Tapped(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                await Shell.Current.GoToAsync($"///settings");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"err: {ex.Message}");
+            }
         }
     }
 }

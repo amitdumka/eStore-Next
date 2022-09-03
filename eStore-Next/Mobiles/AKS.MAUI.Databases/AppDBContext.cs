2e5a4cd3b7c463351b5b7b8cb57c8d5055f15022
Amit Kumar <amitnarayansah@gmail.com>
2022-09-03 22:53:57 +0530

    Login Page is created

---
+++ eStore-Next/Mobiles/AKS.MAUI.Databases/AppDBContext.cs
@@ -0,0 +1,89 @@
+﻿using System.Runtime.CompilerServices;
+using AKS.Shared.Commons.Models;
+using AKS.Shared.Commons.Models.Auth;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace AKS.MAUI.Databases
+{
+    public class MUAIConstant
+    {
+        public const string DatabaseFile = "eStoreDBSqlite.db3";
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFile);
+            }
+        }
+    }
+
+
+    public enum DBType { Local, Azure, Remote, Mango, Others }
+    // All the code in this file is included in all platforms.
+    public class AppDBContext : DbContext
+    {
+        //Remote Azure Db
+        private static readonly string _connectionString = "Data Source=tcp:aprajitaretails.database.windows.net,1433;Initial Catalog=AprajitaRetails_db;User Id=AmitKumar@aprajitaretails;Password=Dumka@@2654";
+        private string DatabasePath { get; set; }
+        DBType _dbType;
+
+        public AppDBContext()
+        {
+
+        }
+
+        public AppDBContext(DBType dBType)
+        {
+            _dbType = dBType;
+            if (dBType == DBType.Local)
+            {
+                DatabasePath = MUAIConstant.DatabasePath;
+                SQLitePCL.Batteries_V2.Init();
+                this.Database.EnsureCreated();
+            }
+        }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            if (_dbType == DBType.Local)
+            {
+                optionsBuilder.UseSqlite($"Filename={DatabasePath}");
+            }
+            else if (_dbType == DBType.Azure)
+            {
+                if (!optionsBuilder.IsConfigured)
+                {
+                    optionsBuilder.UseSqlServer(_connectionString);
+                }
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            if (_dbType == DBType.Azure)
+                modelBuilder.HasDatabaseMaxSize("2 GB");
+        }
+
+        public void ApplyMigrations(AppDBContext context)
+        {
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+        }
+
+
+        //tables
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<LocalUser> LocalUsers { get; set; }
+    }
+}
--- eStore-Next/Mobiles/eStore-Maui/Pages/LoginPage.xaml.cs
+++ eStore-Next/Mobiles/eStore-Maui/Pages/LoginPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace eStore_Maui.Pages;
+﻿using AndroidX.Lifecycle;
+
+namespace eStore_Maui.Pages;
 
 public partial class LoginPage : ContentPage
 {
@@ -6,4 +8,38 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    void DoSignUpClicked(Object sender, EventArgs e)
+    {
+        ////if (viewModel.Validate())
+        //DisplayAlert("Success", "Your account has been created successfully", "OK");
+        //Application.Current.MainPage = new SignUpPage();
+    }
+
+    void DoLoginClicked(Object sender, EventArgs e)
+    {
+        if (UserName.Text == "Admin" && Password.Text == "Admin")
+        {
+
+            Application.Current.MainPage = new AppShell();
+        }
+        else if (DoLogin(UserName.Text, Password.Text))
+        {
+            Application.Current.MainPage = new AppShell();
+        }
+        else
+        {
+            DisplayAlert("Error", "UserName/Password is incorrect!", "OK");
+        }
+    }
+
+    bool DoLogin(string username, string password)
+    {
+        var user = this.BackgroundColor;//viewModel.SignIn(UserName.Text.Trim() + "@eStore.in", Password.Text.Trim());
+        if (user != null)
+        {
+            return true;
+        }
+        else return false;
+
+    }
 }

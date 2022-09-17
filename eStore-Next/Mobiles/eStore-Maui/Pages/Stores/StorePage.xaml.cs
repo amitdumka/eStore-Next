c29b0eb8ed2a1a63273f60a08a09f5ac21877a8f
Amit Kumar <amitnarayansah@gmail.com>
2022-09-17 19:05:38 +0530

    rlv swipe is pending

--- eStore-Next/Mobiles/eStore-Maui/Pages/Stores/StorePage.xaml.cs
+++ eStore-Next/Mobiles/eStore-Maui/Pages/Stores/StorePage.xaml.cs
@@ -10,7 +10,27 @@ public partial class StorePage : ContentPage
 		InitializeComponent();
         Intit();
 	}
-	protected void Intit()
+    //async void OnLeft(object sender, EventArgs e)
+    //{
+    //    await DisplayAlert("Left!", "Left Swipe.", "OK");
+    //}
+    //async void OnRight(object sender, EventArgs e)
+    //{
+    //    await DisplayAlert("Right!", "Right Swipe.", "OK");
+    //}
+    //private void ExportToExcel_Clicked(object sender, EventArgs e)
+    //{
+    //    DataGridExcelExportingController excelExport = new DataGridExcelExportingController();
+    //    var excelEngine = excelExport.ExportToExcel(this.dataGrid);
+    //    var workbook = excelEngine.Excel.Workbooks[0];
+    //    MemoryStream stream = new MemoryStream();
+    //    workbook.SaveAs(stream);
+    //    workbook.Close();
+    //    excelEngine.Dispose();
+
+    //    //.Forms.DependencyService.Get<ISave>().Save("DataGrid.xlsx", "application/msexcel", stream);
+    //}
+    protected void Intit()
 	{
         ColumnCollection gridColumns = new ColumnCollection();
         gridColumns.Add(new DataGridTextColumn() { HeaderText = "Code", MappingName = "StoreCode" });

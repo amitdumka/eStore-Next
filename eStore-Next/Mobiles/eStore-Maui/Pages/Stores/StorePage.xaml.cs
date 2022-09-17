3eec01fdacbf0112e9c776ee4aaa9307ea5b3e3c
Amit Kumar <amitnarayansah@gmail.com>
2022-09-17 19:39:41 +0530

    tapping event on grid

--- eStore-Next/Mobiles/eStore-Maui/Pages/Stores/StorePage.xaml.cs
+++ eStore-Next/Mobiles/eStore-Maui/Pages/Stores/StorePage.xaml.cs
@@ -5,11 +5,12 @@ namespace eStore_Maui.Pages.Stores;
 
 public partial class StorePage : ContentPage
 {
-	public StorePage()
-	{
-		InitializeComponent();
+    public StorePage()
+    {
+        InitializeComponent();
         Intit();
-	}
+    }
+
     //async void OnLeft(object sender, EventArgs e)
     //{
     //    await DisplayAlert("Left!", "Left Swipe.", "OK");
@@ -31,17 +32,32 @@ public partial class StorePage : ContentPage
     //    //.Forms.DependencyService.Get<ISave>().Save("DataGrid.xlsx", "application/msexcel", stream);
     //}
     protected void Intit()
-	{
+    {
         ColumnCollection gridColumns = new ColumnCollection();
         gridColumns.Add(new DataGridTextColumn() { HeaderText = "Code", MappingName = "StoreCode" });
         gridColumns.Add(new DataGridTextColumn() { HeaderText = "Name", MappingName = "StoreName" });
         gridColumns.Add(new DataGridTextColumn() { HeaderText = "City", MappingName = nameof(Store.City) });
-        gridColumns.Add(new DataGridTextColumn() { HeaderText = "Date", MappingName = nameof(Store.BeginDate), Format="dd/MMM/yyyy"  });
+        gridColumns.Add(new DataGridTextColumn() { HeaderText = "Date", MappingName = nameof(Store.BeginDate), Format = "dd/MMM/yyyy" });
         gridColumns.Add(new DataGridTextColumn() { HeaderText = "Zip", MappingName = nameof(Store.ZipCode) });
         gridColumns.Add(new DataGridCheckBoxColumn() { HeaderText = "", MappingName = nameof(Store.IsActive) });
         this.dataGrid.Columns = gridColumns;
-		
+    }
+    private async void dataGrid_CellDoubleTapped(object sender, DataGridCellDoubleTappedEventArgs e)
+    {
+        var rowIndex = e.RowColumnIndex.RowIndex;
+        var rowData = (Store)e.RowData;
+        var columnIndex = e.RowColumnIndex.ColumnIndex;
+        var column = e.Column;
 
-
+        await DisplayAlert("Double!", $"{e.Column.MappingName}: {rowData.StoreName} ", "OK");
+    }
+    private async void dataGrid_CellLongPress(object sender, DataGridCellLongPressEventArgs e)
+    {
+        var rowIndex = e.RowColumnIndex.RowIndex;
+        var rowData = (Store)e.RowData;
+        var columnIndex = e.RowColumnIndex.ColumnIndex;
+        var column = e.Column;
+        
+        await DisplayAlert("Long!", $"{e.Column.MappingName}: {rowData.StoreName} ", "OK");
     }
 }

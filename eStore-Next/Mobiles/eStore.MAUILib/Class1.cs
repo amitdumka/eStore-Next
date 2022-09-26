8a93a699b9a2803835660c3ad57de3d3a38c4e40
Amit Kumar <amitnarayansah@gmail.com>
2022-09-27 02:32:35 +0530

    adding final lib

---
+++ eStore-Next/Mobiles/eStore.MAUILib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace eStore.MAUILib
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ eStore-Next/Mobiles/eStore.MAUILib/Platforms/Android/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace eStore.MAUILib
+{
+    // All the code in this file is only included on Android.
+    public class PlatformClass1
+    {
+    }
+}

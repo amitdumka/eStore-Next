c442b7cd9a7c9a82004a3f381c978da7cc0b0165
Amit Kumar <amitnarayansah@gmail.com>
2022-09-27 21:20:30 +0530

    login working in new unified prj

--- eStore-Next/Mobiles/eStore.MAUILib/DataModels/Auth/AuthDataModel.cs
+++ eStore-Next/Mobiles/eStore.MAUILib/DataModels/Auth/AuthDataModel.cs
@@ -1,5 +1,7 @@
-﻿using AKS.Shared.Commons.Models.Auth;
+﻿using AKS.Shared.Commons.Models;
+using AKS.Shared.Commons.Models.Auth;
 using eStore.MAUILib.DataModels.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace eStore.MAUILib.DataModels.Auth
 {
@@ -23,9 +25,19 @@ namespace eStore.MAUILib.DataModels.Auth
             throw new NotImplementedException();
         }
 
-        public override Task<List<User>> GetItemsAsync(string storeid)
+        public override async Task<List<User>> GetItemsAsync(string storeid)
         {
-            throw new NotImplementedException();
+            switch (Mode)
+            {
+                case DBType.Local:
+                    return await _localDb.Users.ToListAsync();
+                case DBType.Azure:
+                    return await _azureDb.Users.ToListAsync();
+
+                default:
+                    return null;
+
+            }
         }
 
         public override List<int> GetYearList(string storeid)
@@ -38,9 +50,122 @@ namespace eStore.MAUILib.DataModels.Auth
             throw new NotImplementedException();
         }
 
-        public override Task<bool> InitContext()
+        public override async Task<bool> InitContext() => Connect();
+
+        public async Task<Store> GetStore(string sid)
         {
-            throw new NotImplementedException();
+            switch (Mode)
+            {
+                case DBType.Local:
+                    return await _localDb.Stores.FindAsync(sid);
+                case DBType.Azure:
+                    return await _azureDb.Stores.FindAsync(sid);
+
+                default:
+                    return null;
+
+            }
         }
+        public User SignIn(string userName, string password)
+        {
+            User user = null;
+            switch (Mode)
+            {
+                case DBType.Local:
+                    user = _localDb.Users.Where(c => c.UserName == userName && c.Password == password).FirstOrDefault();
+
+                    break;
+                case DBType.Azure:
+                    user = _azureDb.Users.Where(c => c.UserName == userName && c.Password == password).FirstOrDefault();
+                    break;
+                case DBType.API:
+                    break;
+                default:
+                    break;
+            }
+
+            return user;
+        }
+        /// <summary>
+        /// Change Password
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="oldPassword"></param>
+        /// <param name="newPassword"></param>
+        /// <returns></returns>
+        public async Task<bool> PasswordChangeAsync(string userName, string oldPassword, string newPassword)
+        {
+            User user;
+            //TODO: SendInfo or Return in ReturnType class to manage return infomation along with error
+            user = _localDb.Users.Where(c => c.UserName == userName && c.Password == oldPassword).FirstOrDefault();
+
+            switch (Mode)
+            {
+                case DBType.Local:
+                    user = _localDb.Users.Where(c => c.UserName == userName && c.Password == oldPassword).FirstOrDefault();
+                    break;
+                case DBType.Azure:
+                    user = _azureDb.Users.Where(c => c.UserName == userName && c.Password == oldPassword).FirstOrDefault();
+                    break;
+                default:
+                    break;
+            }
+
+            if (user != null)
+            {
+                // TODO: Send info that old password is not matched or user not found!.
+                user.Password = newPassword;
+                user = await SaveAsync(user, false);
+                return (user != null);
+            }
+            else
+                return false;
+
+        }
+
+        public async Task<bool> SignUpAsync(User user) => (await SaveAsync(user, true)) != null;
+
+        public async Task<bool> SyncRemote(DBType dbType)
+        {
+            switch (dbType)
+            {
+                case DBType.Azure:
+                    if (_azureDb == null)
+                    {
+                        _azureDb = new AKS.MAUI.Databases.AppDBContext();
+                    }
+                    var users = _azureDb.Users.Where(c => c.StoreId == StoreCode).ToList();
+                    int newUser = 0;
+                    foreach (var user in users)
+                    {
+                        if (!_localDb.Users.Any(c => c.UserName == user.UserName))
+                        {
+                            _localDb.Users.Add(user);
+                            newUser++;
+                        }
+                    }
+                    int count = await _localDb.SaveChangesAsync();
+                    return (newUser == count);
+                    
+
+                case DBType.API:
+                    //RemoteSingleServer server = new RemoteSingleServer("", "User");
+                    //var users = await server.GetByUrl<List<User>>(WebAPI.APIBase + WebAPI.Users);
+                    //foreach (var user in users)
+                    //{
+                    //    user.UserId = 0; //TODO: remove is leagace.
+                    //}
+                    //_localDb.Users.AddRange(users);
+                    //return _localDb.SaveChanges() > 0;
+                    break;
+                case DBType.Mango: break;
+                case DBType.Remote: break;
+                default:
+                    break;
+            }
+
+            return false;
+        }
+
     }
 }
--- eStore-Next/Mobiles/eStore.MAUILib/ViewModels/Auth/AuthViewModel.cs
+++ eStore-Next/Mobiles/eStore.MAUILib/ViewModels/Auth/AuthViewModel.cs
@@ -1,11 +1,16 @@
-﻿using AKS.Shared.Commons.Models.Auth;
+﻿using AKS.Shared.Commons.Data.Helpers.Auth;
+using AKS.Shared.Commons.Models.Auth;
 using AKS.Shared.Commons.Ops;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using eStore.DatabaseSyncService.Services;
 using eStore.MAUILib.DataModels.Auth;
+using eStore.MAUILib.Helpers;
+using eStore.MAUILib.Services;
 using eStore.MAUILib.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +19,42 @@ namespace eStore.MAUILib.ViewModels.Auth
 {
     public partial class AuthViewModel : BaseViewModel<User, AuthDataModel>
     {
+        #region PropertyFields
         [ObservableProperty]
-        private Shell _appShell;
+        //[NotifyPropertyChangedRecipients]
+        [NotifyDataErrorInfo]
+        [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
+        [EmailAddress]
+        private string _userName;
+
+        [ObservableProperty]
+        //[NotifyPropertyChangedRecipients]
+        [NotifyDataErrorInfo]
+        [Required]
+        [MinLength(4)]
+        [MaxLength(12)]
+        private string _password;
+
+        [ObservableProperty]
+        private static Shell _appShell;
+
+        [ObservableProperty]
+        private string _guestName;
+
+        #endregion PropertyFields
 
         [ObservableProperty]
         private bool _isEnableSign;
+
+        public AuthViewModel()
+        {
+            DataModel = new AuthDataModel(ConType.Hybrid);
+            InitViewModel();
+        }
+
+
         protected override void AddButton()
         {
             throw new NotImplementedException();
@@ -29,9 +65,17 @@ namespace eStore.MAUILib.ViewModels.Auth
             throw new NotImplementedException();
         }
 
+
         protected override void InitViewModel()
         {
-            throw new NotImplementedException();
+            DataModel.Connect();
+            DataModel.Mode = DBType.Local;
+            //UpdateEntities(await DataModel.GetItems());
+            Title = "Login";
+            GuestName = "No user ";
+            DefaultSortedColName = nameof(User.UserName);
+            UserName = "Alok@eStore.in";
+            Password = "Alok";
         }
 
         protected override void RefreshButton()
@@ -39,9 +83,14 @@ namespace eStore.MAUILib.ViewModels.Auth
             throw new NotImplementedException();
         }
 
-        protected override void UpdateEntities(List<User> values)
+        protected override void UpdateEntities(List<User> users)
         {
-            throw new NotImplementedException();
+            if (Entities == null) Entities = new System.Collections.ObjectModel.ObservableCollection<User>();
+            foreach (var user in users)
+            {
+                Entities.Add(user);
+            }
+            RecordCount = Entities.Count();
         }
 
         public async void SyncLocal()
@@ -50,16 +99,89 @@ namespace eStore.MAUILib.ViewModels.Auth
             {
                 if (!DatabaseStatus.VerifyLocalStatus())
                 {
-                   await DatabaseStatus.SyncInitial();
-                    IsEnableSign = CurrentSession.LocalStatus=true;
+                    await DatabaseStatus.SyncInitial();
+                    IsEnableSign = CurrentSession.LocalStatus = true;
                 }
-                
-                IsEnableSign = CurrentSession.LocalStatus=true;
+
+                IsEnableSign = CurrentSession.LocalStatus = true;
             }
 
             CurrentSession.LocalStatus = true;
-            IsEnableSign = CurrentSession.LocalStatus=true;
+            IsEnableSign = CurrentSession.LocalStatus = true;
 
         }
+
+        #region Login
+
+        [RelayCommand]
+        private void ShowErrors()
+        {
+            string message = string.Join(Environment.NewLine, GetErrors().Select(e => e.ErrorMessage));
+            Notify.NotifyLong(message);
+        }
+
+        [RelayCommand]
+        private async Task<bool> SignIn()
+        {
+            ValidateAllProperties();
+            if (HasErrors)
+            {
+                ShowErrors();
+                return false;
+            }
+            else
+            {
+                var user = DataModel.SignIn(_userName, _password);
+
+                if (user != null)
+                {
+                    var store = await DataModel.GetStore(user.StoreId);
+                    CurrentSession.StoreCode = user.StoreId;
+                    CurrentSession.UserName = user.UserName + "#TESTING";
+                    CurrentSession.GuestName = user.GuestName;
+                    CurrentSession.IsLoggedIn = true;
+                    CurrentSession.LoggedTime = DateTime.Now;
+                    CurrentSession.UserType = user.UserType;
+                    CurrentSession.EmployeeId = string.IsNullOrEmpty(user.EmployeeId) ? null : user.EmployeeId;
+                    CurrentSession.Role = AuthHelper.GetPermission(user.UserType);
+                    CurrentSession.Perimissions = AuthHelper.GetPermission(CurrentSession.Role);
+                    GuestName = user.GuestName;
+
+                    Notify.NotifyVLong($"Welcome, {GuestName}!, Now you can operate in , {user.UserType}, mode. ");
+
+                    if (store != null)
+                    {
+                        CurrentSession.Address = store.City + "\t" + store.State;
+                        CurrentSession.TaxNumber = store.GSTIN;
+                        CurrentSession.StoreName = store.StoreName;
+                        CurrentSession.PhoneNo = store.StorePhoneNumber;
+                        CurrentSession.CityName = store.City;
+                        Notify.NotifyLong($"Welcome {CurrentSession.GuestName}, from {CurrentSession.StoreName}");
+                        Application.Current.MainPage = AppShell;
+                        return true;
+                    }
+                }
+                Notify.NotifyVLong($"User {UserName} not Found ....");
+
+                return false;
+            }
+        }
+
+        [RelayCommand]
+        private async Task<bool> SignOut()
+        {
+            CurrentSession.Clear();
+            return true;
+        }
+
+        [RelayCommand]
+        private async Task<bool> SignUP()
+        {
+            Notify.NotifyVLong($"Welcome {CurrentSession.GuestName}, from {CurrentSession.StoreName}");
+            Application.Current.MainPage = AppShell;
+            return true;
+        }
+
+        #endregion
     }
 }

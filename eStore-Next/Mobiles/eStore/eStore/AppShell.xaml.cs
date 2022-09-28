3b43e1caa13034aeab0e12ec1aaea4752cfcc3a1
Amit Kumar <amitsha.2407@gmail.com>
2022-09-28 17:31:34 +0530

    changing due to error

---
+++ eStore-Next/Mobiles/eStore/eStore/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace eStore;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ eStore-Next/Mobiles/eStore/eStore/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace eStore;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}

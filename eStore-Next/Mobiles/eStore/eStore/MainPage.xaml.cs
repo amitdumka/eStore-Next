f85914463214c7be4eec0c3e3adfede1dbdfd8b2
Amit Kumar <amitnarayansah@gmail.com>
2022-10-02 03:10:40 +0530

    list widget created

--- eStore-Next/Mobiles/eStore/eStore/MainPage.xaml.cs
+++ eStore-Next/Mobiles/eStore/eStore/MainPage.xaml.cs
@@ -1,24 +1,36 @@
-﻿namespace eStore;
+﻿using static eStore.Views.ListWidget;
+
+namespace eStore;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public List<ItemList> SourceList = new(){
+        new ItemList {Title="Yearly", Description="2000" },
+        new ItemList { Title = "Monthly", Description = "2000" },
+        new ItemList
+        {
+            Title = "Qutarly",
+            Description = "2000"
+        },
+    };
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    public MainPage()
+    {
+        InitializeComponent();
+        LWA.ItemData = SourceList;
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 }
-

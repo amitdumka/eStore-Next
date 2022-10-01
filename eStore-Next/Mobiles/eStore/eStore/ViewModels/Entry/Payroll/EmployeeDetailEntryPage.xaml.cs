a7e11f9ea73a663f6e232b76926123fdfa4177a6
Amit Kumar <amitnarayansah@gmail.com>
2022-10-01 21:24:52 +0530

    clean up

--- eStore-Next/Mobiles/eStore/eStore/ViewModels/Entry/Payroll/EmployeeDetailEntryPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace eStore.ViewModels.Entry.Payroll;
-
-public partial class EmployeeDetailEntryPage : ContentPage
-{
-	public EmployeeDetailEntryPage()
-	{
-		InitializeComponent();
-	}
-}
--- eStore-Next/Mobiles/eStore/eStore/ViewModels/Entry/Payroll/EmployeeEntryPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace eStore.ViewModels.Entry.Payroll;
-
-public partial class EmployeeEntryPage : ContentPage
-{
-	public EmployeeEntryPage()
-	{
-		InitializeComponent();
-	}
-}

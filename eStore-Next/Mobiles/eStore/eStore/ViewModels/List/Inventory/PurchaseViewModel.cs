5856ddf865632e70ce2126e1260dc7d155afda7e
Amit Kumar <amitnarayansah@gmail.com>
2022-10-04 10:57:19 +0530

    viewmodel inventory

---
+++ eStore-Next/Mobiles/eStore/eStore/ViewModels/List/Inventory/PurchaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AKS.Shared.Commons.Models.Inventory;
+using eStore.MAUILib.DataModels.Inventory;
+using eStore.MAUILib.ViewModels.Base;
+
+namespace eStore.ViewModels.List.Inventory
+{
+    public class PurchaseViewModel:BaseViewModel<PurchaseProduct, PurchaseDataModel>
+		{ }
+}
+
---
+++ eStore-Next/Mobiles/eStore/eStore/ViewModels/List/Inventory/SaleViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using AKS.Shared.Commons.Models.Inventory;
+using eStore.MAUILib.DataModels.Inventory;
+using eStore.MAUILib.ViewModels.Base;
+using Syncfusion.Maui.DataGrid;
+
+namespace eStore.ViewModels.List.Inventory
+{
+    public class SaleViewModel : BaseViewModel<ProductSale, SaleDataModel>
+    {
+        public SaleViewModel()
+        {
+        }
+
+        protected override void AddButton()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DeleteButton()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InitViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RefreshButton()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<ColumnCollection> SetGridCols()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class StockViewModel { }
+	public class ProductCategoryViewModel { }
+	public class ProductViewModel { }
+}
+

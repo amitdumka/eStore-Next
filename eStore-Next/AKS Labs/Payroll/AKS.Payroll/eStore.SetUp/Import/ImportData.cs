c8b49a1bcd9beff2667256671ca9fd74bf48c562
Amit Kumar <amitnarayansah@gmail.com>
2022-10-10 00:52:12 +0530

    import export

---
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/eStore.SetUp/Import/ImportData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStore.SetUp.Import
+{
+    public class ImportData
+    {
+        public void ImportPurchaseInvoice(){ }
+        public void ImportSaleInvoice() { }
+        public void ImportMSDyncSaleInvoice() { }
+
+
+        private void ProductItem() { }
+        private void StockItem() { }
+        private void Category() { }
+        private void SaleItem() { }
+        private void PurchaseItem() { }
+
+        //TODO: 
+        // Convert everthing to JSON
+        // store all josn file to a directory
+        // befercate all items to each category of data segment and store in json. 
+        // Convert everything to particular object and store in json. 
+        // read data and store in database.
+
+
+    }
+}

2a32567837e0b2888d30c1c006ab757b3cf0ca18
Amit Kumar <amitsha.2407@gmail.com>
2022-12-09 11:45:03 +0530

    asd

---
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/eStore.SetUp/StockReview/CurrentStock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStore.SetUp.StockReview
+{
+    public class PhyStockHistory
+    {
+        public string StoreId { get; set; }
+        public int PhyStockHistoryId { get; set; }
+        public DateTime OnDate { get; set; }
+        public decimal Qty { get; set; }
+        public string Remark { get; set; }
+    }
+    public class StockAudit
+    {
+
+
+    }
+}

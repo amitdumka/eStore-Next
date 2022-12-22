edb03fbe9730cd74f80b0b5b63ee1f36f4bc2192
Amit Kumar <amitsha.2407@gmail.com>
2022-12-22 20:37:26 +0530

    stock history

--- eStore-Next/AKS Labs/Payroll/AKS.Payroll/eStore.SetUp/StockReview/CurrentStock.cs
+++ eStore-Next/AKS Labs/Payroll/AKS.Payroll/eStore.SetUp/StockReview/CurrentStock.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AKS.Shared.Commons.Models.Inventory;
 
 namespace eStore.SetUp.StockReview
 {
@@ -11,12 +7,55 @@ namespace eStore.SetUp.StockReview
         public string StoreId { get; set; }
         public int PhyStockHistoryId { get; set; }
         public DateTime OnDate { get; set; }
+        public string Barcode { get; set; }
+        public Category Category { get; set; }
         public decimal Qty { get; set; }
         public string Remark { get; set; }
     }
+
     public class StockAudit
     {
+    }
 
+    public class PurchaseHistory
+    {
+        public int Id { get; set; }
+        public string Barcode { get; set; }
+        public string InvoiceNo { get; set; }
+        public DateTime OnDate { get; set; }
+        public decimal Qty { get; set; }
+        public decimal Rate { get; set; }
+        public decimal MRP { get; set; }
+        public decimal Tax { get; set; }
+        public string Location { get; set; }
+        public bool IsStockTransfer { get; set; }
+    }
 
+    public class SaleHistory
+    {
+        public int Id { get; set; }
+        public string Barcode { get; set; }
+        public string InvoiceNo { get; set; }
+        public DateTime OnDate { get; set; }
+        public decimal Qty { get; set; }
+        public decimal Rate { get; set; }
+        public decimal SaleValue { get; set; }
+        public decimal TaxValue { get; set; }
+        public string Location { get; set; }
+        public bool IsStockTransfer { get; set; }
+        public bool IsManual { get; set; }
+    }
+
+    public class ProfitLoss
+    {
+        public string Barcode { get; set; }
+        public decimal Rate { get; set; }
+        public decimal PurchaseQty { get; set; }
+        public decimal SoldQty { get; set; }
+        public decimal StockInHand { get; set; }
+        public decimal PurchaseValue { get; set; }
+        public decimal SaleValue { get; set; }
+        public decimal StockValue { get; set; }
+        public decimal ProfitAmount { get; set; }
     }
 }
